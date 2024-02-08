39071205d18cf8169da6440030403383a3ef23e5
Judanaelle <sanaelle431@gmail.com>
2024-02-08 09:40:32 -0500

    update

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,1 @@
-hi
+print(word) world
--- Testing/test.py
+++ Testing/test.py
@@ -1,1 +1,3 @@
-hi my name is jeaicha sylvain and i'm haitian
+print("Hi my name is Jeaicha sylvain and i'm haitian.")
+print("I'm an only child born in 2003 and i speak french")
+print("My favorite activities are music,eating movies and spending time with my family and friends")
