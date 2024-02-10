9f268c5e692e3b6a57b158d6f1a358dc363ac2a0
Judanaelle <sanaelle431@gmail.com>
2024-02-10 16:41:11 -0500

    testing - david

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -2,4 +2,6 @@ print("word")
 
 list = "test"
 
-print(list)
+print("my name is Jeaicha Sylvain")
+print("I was born in Haiti and I'm an only child")
+print("my favorite activities are movies and musics")
